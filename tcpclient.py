4ecb9d88f5bbd47c762572eb2cdb306b8d0d4646
chunnnn0114 <pinchun0114@gmail.com>
2025-10-04 00:45:06 +0800

    Create tcpclient.py

---
+++ tcpclient.py
@@ -0,0 +1,21 @@
+import socket
+
+SERVER_IP = "192.168.0.87"  # 伺服端 IP (如果跨電腦，改成伺服端的 IP)
+PORT = 8888 # 必須跟伺服端一致
+
+# 建立 TCP socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 嘗試連線到伺服端
+client.connect((SERVER_IP, PORT))
+
+# 傳送測試訊息
+msg = "Hello from client"
+client.sendall(msg.encode())
+
+# 接收伺服端的回覆
+data = client.recv(1024).decode()
+print("Received from server:", data)
+
+# 關閉連線
+client.close()
