e1a1f1e603fee09063baa7b150f40a35fcf701bc
0901eating-a11y <0901eating@gmail.com>
2025-10-06 16:55:19 +0800

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -1,7 +1,7 @@
 # udp_server.py
 import socket
 
-HOST = "127.0.0.1"   # 本機 (loopback)
+HOST = "0.0.0.0"   # 監聽所有網卡
 PORT = 9999          # 建議不要跟 TCP 共用同一個 port，改成 9999 比較清楚
 
 # 建立 UDP socket (SOCK_DGRAM)
@@ -23,4 +23,5 @@ print("Sent to client:", reply)     # 顯示已經回覆什麼
 
 # UDP 是無連線協定，用完就可以關閉
 server.close()
+
 print("UDP Server closed.")         # 提示程式結束
